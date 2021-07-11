672f54534dcabb42b9ead28a1663b8f6b6d40afb
Andre Julius <noromoron@gmail.com>
2021-07-11 14:09:40 +0200

    Logout using the api

--- main.js
+++ main.js
@@ -1,20 +1,50 @@
 async function logout(e, logout_elem, access_token, token_type) {
     e.preventDefault();
-    localStorage.clear();
-    logout_elem.style.display = "none";
 
     try {
-        let response = await fetch("https://discord.com/api/oauth2/token/revoke", {
-            headers: {
-                authorization: `${token_type} ${access_token}`,
-            },
-        });
-        let json = await response.json();
-        console.log(json);
-        console.log("logged out");
+        let result = await apiLogout(access_token, token_type);
+        localStorage.clear();
+        logout_elem.style.display = "none";
+        console.log(result);
     } catch (err) {
         console.error(err);
     }
+}
+
+// gets all the pictures already liked by this user
+async function apiLogout(accessToken, tokenType) {
+    return await _api("logout", accessToken, tokenType, {});
+}
+
+
+async function _api(action, accessToken, tokenType, options) {
+    let params = new URLSearchParams();
+    params.set("access_token", accessToken);
+    params.set("token_type", tokenType);
+
+    let url = api_url;
+    switch (action) {
+        case "like":
+            url += `/like/${options.entry}`;
+            break;
+        case "unlike":
+            url += `/unlike/${options.entry}`;
+            break;
+        case "likes-by-user":
+            url += "/likes/by-user";
+            break;
+        case "logout":
+            url += "/logout";
+            break;
+        default:
+            throw "Unknown action";
+    }
+    url += `/?${params.toString()}`;
+    console.log("Making request to: ", url);
+
+    let result = await fetch(url);
+    let json = await result.json();
+    return json;
 }
 
 // consider this the main function
